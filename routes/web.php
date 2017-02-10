6c5ab09d7210b0954b15c1222665976adcd953aa
sally cohen <saliloucohen18@yahoo.fr>
2017-02-10 12:39:40 +0100

    Hello world

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function () {
+    return view('welcome');
+});
 Auth::routes();
 Route::get('/home', 'HomeController@index');
 Route::resource('/article', 'ArticleController');
